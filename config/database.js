6f52d67db032605272acfd015ca5f2f3e4c340d1
Harley Lin <harley9387@gmail.com>
2025-06-13 20:51:14 +1200

    change database to PG

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const fs = require("fs");
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env("DATABASE_CLIENT", "postgres");
 
   const connections = {
     mysql: {
@@ -33,7 +34,8 @@ module.exports = ({ env }) => {
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
+          // ca: env('DATABASE_SSL_CA', undefined),
+          ca: fs.readFileSync(path.resolve(__dirname, '../ca-certificate.crt')).toString(),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
